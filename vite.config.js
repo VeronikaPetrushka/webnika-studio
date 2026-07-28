e78570dfee3bb5cbb6ca5c68e6ee8200986fbdee
Veronika <veronikapetrushka@gmail.com>
2026-07-28 22:51:05 +0200

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname:
+        "https://veronikapetrushka.github.io/webnika-studio/",
+      dynamicRoutes: ["/"],
+      readable: true,
+    }),
+  ],
+
   base: "/webnika-studio/",
 });

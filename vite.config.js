5c6c2ee6a4866e99e5118fa85acdc31459eb1576
Veronika <veronikapetrushka@gmail.com>
2026-07-28 23:10:41 +0200

    Fix sitemap URLs

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,25 @@ import react from "@vitejs/plugin-react";
 import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
+  base: "/webnika-studio/",
+
   plugins: [
     react(),
+
     Sitemap({
-      hostname:
-        "https://veronikapetrushka.github.io/webnika-studio/",
-      dynamicRoutes: ["/"],
+      hostname: "https://veronikapetrushka.github.io",
+
+      dynamicRoutes: [
+        "/webnika-studio/",
+      ],
+
+      exclude: [
+        "/",
+        "/google8108b5e11dc186b5",
+        "/google8108b5e11dc186b5.html",
+      ],
+
       readable: true,
     }),
   ],
-
-  base: "/webnika-studio/",
 });
